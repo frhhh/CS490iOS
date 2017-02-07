df32aaa5f8c5eeef22477184861fda76b0e278d8
Frank Hu <hu353@purdue.edu>
2017-02-06 21:25:49 -0500

    Upload main file
    Upload main file

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Frank Hu on 2017/2/6.
+//  Copyright © 2017年 Weichu Hu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
